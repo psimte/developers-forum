dc38ebfdd455c6b2323f33d083d7dccc8006cfd3
Lal Simte <lalsimte@gmail.com>
2020-04-03 03:01:31 +0530

    post reducer and action

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 
 //Redux
@@ -51,6 +52,7 @@ const App = () =>{
                      <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
                      <PrivateRoute exact path="/add-experience" component={ AddExperience } />
                      <PrivateRoute exact path="/add-education" component={ AddEducation } />
+                     <PrivateRoute exact path="/posts" component={ Posts } />
                 </Switch>
             </section>
         </Fragment>
